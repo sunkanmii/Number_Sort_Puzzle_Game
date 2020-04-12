a206ea709df4bd270a578f5e55ab16901cfdc8c8
Fafowora Olasunkanmi <sunkanmifafowora@gmail.com>
2020-04-12 02:23:53 +0100

    Dynamic input done

--- src/main.js
+++ src/main.js
@@ -3,11 +3,57 @@
 class App {
 
     constructor(posId) {
-        this.default = 15;
         this.checkBut = document.querySelector("#checkBut");
         this.checkBut.addEventListener("click", () => {
             this.checkSorted();
+        });
+        this.selectedRows = 0;
+        this.numberMax = this.selectedRows ** 2;
+        this.gameStartSec = document.querySelector(".game-container");
+    }
+
+    generateSpans() {
+        let span = document.createElement("span");
+        const amountSec = document.querySelector("#amount");
+        const mainGame = document.querySelector("#mainGame");
+        this.okay = amountSec.querySelector("#okay");
+        this.rowSec = amountSec.querySelector("#row-by-column");
+        this.selectedRows = Number(this.rowSec.options[this.rowSec.selectedIndex].value);
+        this.numberMax = this.selectedRows ** 2 - 1;
+
+        let mySet = this.randomMize(this.selectedRows);
+        let tempVal = "";
+        mySet = Array.from(mySet);
+        amountSec.style.display = "none";
+
+        mySet.forEach((ele) => {
+            span = document.createElement("span");
+            tempVal = document.createTextNode(ele);
+            span.appendChild(tempVal);
+            this.gameStartSec.appendChild(span);
         })
+        
+        span = document.createElement("span");
+        tempVal = "";
+        span.setAttribute("id", "space");
+        this.gameStartSec.appendChild(span);
+        this.gameStartSec.style.display = "grid";
+        mainGame.style.display = "grid";
+
+        switch (this.selectedRows) {
+            case 4:
+                this.gameStartSec.style.gridTemplateColumns = "4rem 4rem 4rem 4rem";
+                break;
+            case 5:
+                this.gameStartSec.style.gridTemplateColumns = "4rem 4rem 4rem 4rem 4rem";
+                break;
+            case 6:
+                this.gameStartSec.style.gridTemplateColumns = "4rem 4rem 4rem 4rem 4rem 4rem";
+                break;
+            case 7:
+                this.gameStartSec.style.gridTemplateColumns = "4rem 4rem 4rem 4rem 4rem 4rem 4rem";
+                break;
+        }
     }
 
     keyPress() {
@@ -18,43 +64,43 @@ class App {
         const stylesheet = document.styleSheets[0];
 
         document.onkeydown = (event) => {
-            if (gameSec.children[this.default+1] != null && gameSec.children[this.default].hasAttribute("title") === false && event.keyCode === 37) {
+            if (gameSec.children[this.numberMax + 1] != null && gameSec.children[this.numberMax].hasAttribute("title") === false && event.keyCode === 37) {
 
-                gameSec.children[this.default].removeAttribute("id");
-                this.default++;
+                gameSec.children[this.numberMax].removeAttribute("id");
+                this.numberMax++;
 
-                temp = gameSec.children[this.default].textContent;
-                gameSec.children[this.default-1].textContent = temp;
+                temp = gameSec.children[this.numberMax].textContent;
+                gameSec.children[this.numberMax - 1].textContent = temp;
 
-                gameSec.children[this.default].textContent = '';
-                gameSec.children[this.default].setAttribute("id", "space");
-            } else if (gameSec.children[this.default+4] != null && event.keyCode === 38) {
-                gameSec.children[this.default].removeAttribute("id");
-                this.default += 4;
+                gameSec.children[this.numberMax].textContent = '';
+                gameSec.children[this.numberMax].setAttribute("id", "space");
+            } else if (gameSec.children[this.numberMax + this.selectedRows] != null && event.keyCode === 38) {
+                gameSec.children[this.numberMax].removeAttribute("id");
+                this.numberMax += this.selectedRows;
 
-                temp = gameSec.children[this.default].textContent;
-                gameSec.children[this.default-4].textContent = temp;
+                temp = gameSec.children[this.numberMax].textContent;
+                gameSec.children[this.numberMax - this.selectedRows].textContent = temp;
 
-                gameSec.children[this.default].textContent = '';
-                gameSec.children[this.default].setAttribute("id", "space");
-            } else if (gameSec.children[this.default-1] != null && gameSec.children[this.default-1].hasAttribute("title") === false && event.keyCode === 39) {
-                gameSec.children[this.default].removeAttribute("id");
-                this.default -= 1;
+                gameSec.children[this.numberMax].textContent = '';
+                gameSec.children[this.numberMax].setAttribute("id", "space");
+            } else if (gameSec.children[this.numberMax - 1] != null && gameSec.children[this.numberMax - 1].hasAttribute("title") === false && event.keyCode === 39) {
+                gameSec.children[this.numberMax].removeAttribute("id");
+                this.numberMax -= 1;
 
-                temp = gameSec.children[this.default].textContent;
-                gameSec.children[this.default+1].textContent = temp;
+                temp = gameSec.children[this.numberMax].textContent;
+                gameSec.children[this.numberMax + 1].textContent = temp;
 
-                gameSec.children[this.default].textContent = '';
-                gameSec.children[this.default].setAttribute("id", "space");
-            } else if (gameSec.children[this.default-4] != null && event.keyCode === 40) {
-                gameSec.children[this.default].removeAttribute("id");
-                this.default -= 4;
+                gameSec.children[this.numberMax].textContent = '';
+                gameSec.children[this.numberMax].setAttribute("id", "space");
+            } else if (gameSec.children[this.numberMax - this.selectedRows] != null && event.keyCode === 40) {
+                gameSec.children[this.numberMax].removeAttribute("id");
+                this.numberMax -= this.selectedRows;
 
-                temp = gameSec.children[this.default].textContent;
-                gameSec.children[this.default+4].textContent = temp;
+                temp = gameSec.children[this.numberMax].textContent;
+                gameSec.children[this.numberMax + this.selectedRows].textContent = temp;
 
-                gameSec.children[this.default].textContent = '';
-                gameSec.children[this.default].setAttribute("id", "space");
+                gameSec.children[this.numberMax].textContent = '';
+                gameSec.children[this.numberMax].setAttribute("id", "space");
             } else {
                 console.log("dead end");
             }
@@ -64,7 +110,7 @@ class App {
     iterateProp() {
         const gameSecChildren = document.querySelector(".game-container").children;
 
-        for (let i = 3; i < gameSecChildren.length; i += 4) {
+        for (let i = 3; i < gameSecChildren.length; i += this.selectedRows) {
             gameSecChildren[i].setAttribute("title", "");
         }
     }
@@ -97,12 +143,11 @@ class App {
             let newVal = Math.floor(Math.random() * ((val) - 1)) + 1;
             if (newSet.has(newVal)) {
                 newVal = Math.floor(Math.random() * ((val) - 1)) + 1;
-            }
-            else{
+            } else {
                 newSet.add(newVal);
             }
 
-            if(newSet.size === val - 1){
+            if (newSet.size === val - 1) {
                 i = 0;
                 break;
             }
@@ -117,12 +162,12 @@ start.iterateProp();
 start.keyPress();
 const body = document.querySelector("body");
 
-window.addEventListener("load", () => {
-    let mySet = start.randomMize(4);
-    
-    mySet = Array.from(mySet);
-    const gameSec = document.querySelector(".game-container");
-    mySet.forEach((ele, index) => {
-        gameSec.children[index].textContent = ele; 
-    });
-})
+// window.addEventListener("load", () => {
+//     let mySet = start.randomMize(4);
+
+//     mySet = Array.from(mySet);
+//     const gameSec = document.querySelector(".game-container");
+//     mySet.forEach((ele, index) => {
+//         gameSec.children[index].textContent = ele; 
+//     });
+// })
